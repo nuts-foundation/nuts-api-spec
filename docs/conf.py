00be29e39b41a0de8e1edb62e2b757d7c5f90799
Wout Slakhorst <wout.slakhorst@nedap.com>
2019-05-20 15:40:42 +0200

    conf.py update
    Signed-off-by: Wout Slakhorst <wout.slakhorst@nedap.com>

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information -----------------------------------------------------
 
-project = u'Nuts node RPC specification'
+project = u'Nuts API specification'
 copyright = u'2019, Nuts community'
 author = u'Nuts community'
 
@@ -119,7 +119,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'NutsnodeRPCspecificationdoc'
+htmlhelp_basename = 'NutsAPIspecificationdoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -146,7 +146,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'NutsnodeRPCspecification.tex', u'Nuts node RPC specification Documentation',
+    (master_doc, 'NutsAPIspecification.tex', u'Nuts node RPC specification Documentation',
      u'Nuts community', 'manual'),
 ]
 
@@ -156,7 +156,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'nutsnoderpcspecification', u'Nuts node RPC specification Documentation',
+    (master_doc, 'nutsAPIspecification', u'Nuts node RPC specification Documentation',
      [author], 1)
 ]
 
@@ -167,8 +167,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'NutsnodeRPCspecification', u'Nuts node RPC specification Documentation',
-     author, 'NutsnodeRPCspecification', 'One line description of project.',
+    (master_doc, 'NutsAPIspecification', u'Nuts API specification Documentation',
+     author, 'NutsAPIspecification', 'One line description of project.',
      'Miscellaneous'),
 ]
 
