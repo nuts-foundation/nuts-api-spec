37a7147f5ac6d190d8d097214e61e4094f6a8cea
Wout Slakhorst <wout.slakhorst@nedap.com>
2019-05-20 11:06:12 +0200

    add css
    Signed-off-by: Wout Slakhorst <wout.slakhorst@nedap.com>

--- docs/conf.py
+++ docs/conf.py
@@ -188,3 +188,6 @@ epub_exclude_files = ['search.html']
 
 
 # -- Extension configuration -------------------------------------------------
+
+def setup(app):
+    app.add_stylesheet('css/swagger-ui.css')
